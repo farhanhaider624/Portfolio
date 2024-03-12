3bfe74069175f6577e57cb473bb3796963b8c975
farhanhaider624 <farhanhaider624@gmail.com>
2024-03-13 03:22:22 +0530

    initial commit

---
+++ src/components/Navbar/index.js
@@ -0,0 +1,291 @@
+import React, { useState } from "react";
+import { Link as LinkR } from "react-router-dom";
+import styled, { useTheme } from "styled-components";
+import { DiCssdeck } from "react-icons/di";
+import { FaBars } from "react-icons/fa";
+import { Bio } from "../../data/constants";
+
+const Nav = styled.div`
+  background-color: ${({ theme }) => theme.bg};
+  height: 80px;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  font-size: 1rem;
+  position: sticky;
+  top: 0;
+  z-index: 10;
+  color: white;
+`;
+const ColorText = styled.div`
+  color: ${({ theme }) => theme.primary};
+  font-size: 32px;
+`;
+
+const NavbarContainer = styled.div`
+  width: 100%;
+  max-width: 1200px;
+  padding: 0 24px;
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  font-size: 1rem;
+`;
+const NavLogo = styled(LinkR)`
+  display: flex;
+  align-items: center;
+  width: 80%;
+  padding: 0 6px;
+  font-weight: 500;
+  font-size: 18px;
+  text-decoration: none;
+  color: inherit;
+`;
+
+const NavItems = styled.ul`
+  width: 100%;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  gap: 32px;
+  padding: 0 6px;
+  list-style: none;
+
+  @media screen and (max-width: 768px) {
+    display: none;
+  }
+`;
+
+const NavLink = styled.a`
+  color: ${({ theme }) => theme.text_primary};
+  font-weight: 500;
+  cursor: pointer;
+  transition: all 0.2s ease-in-out;
+  text-decoration: none;
+  &:hover {
+    color: ${({ theme }) => theme.primary};
+  }
+`;
+
+const ButtonContainer = styled.div`
+  width: 80%;
+  height: 100%;
+  display: flex;
+  justify-content: end;
+  align-items: center;
+  padding: 0 6px;
+  @media screen and (max-width: 768px) {
+    display: none;
+  }
+`;
+
+const GithubButton = styled.a`
+  border: 1px solid ${({ theme }) => theme.primary};
+  color: ${({ theme }) => theme.primary};
+  justify-content: center;
+  display: flex;
+  align-items: center;
+  border-radius: 20px;
+  cursor: pointer;
+  padding: 10px 20px;
+  font-size: 16px;
+  font-weight: 500;
+  transition: all 0.6s ease-in-out;
+  text-decoration: none;
+  &:hover {
+    background: ${({ theme }) => theme.primary};
+    color: ${({ theme }) => theme.text_primary};
+  }
+`;
+
+const MobileIcon = styled.div`
+  height: 100%;
+  display: flex;
+  align-items: center;
+  color: ${({ theme }) => theme.text_primary};
+  display: none;
+  @media screen and (max-width: 768px) {
+    display: block;
+  }
+`;
+
+const MobileMenu = styled.ul`
+  width: 100%;
+  display: flex;
+  flex-direction: column;
+  align-items: start;
+  gap: 16px;
+  padding: 0 6px;
+  list-style: none;
+  width: 100%;
+  padding: 12px 40px 24px 40px;
+  background: ${({ theme }) => theme.card_light + 99};
+  position: absolute;
+  top: 80px;
+  right: 0;
+
+  transition: all 0.6s ease-in-out;
+  transform: ${({ open }) => (open ? "translateY(0)" : "translateY(-100%)")};
+  border-radius: 0 0 20px 20px;
+  box-shadow: 0 0 10px 0 rgba(0, 0, 0, 0.2);
+  opacity: ${({ open }) => (open ? "100%" : "0")};
+  z-index: ${({ open }) => (open ? "1000" : "-1000")};
+`;
+
+export const Span = styled.div`
+  padding: 0 4px;
+  font-weight: bold;
+  font-size: 18px;
+`;
+
+const MobileMenuLinks = styled(LinkR)`
+  color: ${({ theme }) => theme.text_primary};
+  font-weight: 500;
+  cursor: pointer;
+  text-decoration: none;
+  transition: all 0.2s ease-in-out;
+  &:hover {
+    color: ${({ theme }) => theme.primary};
+  }
+`;
+
+const ResumeButton = styled.a`
+  -webkit-appearance: button;
+  -moz-appearance: button;
+  appearance: button;
+  text-decoration: none;
+
+  width: 95%;
+  max-width: 300px;
+  text-align: center;
+  padding: 16px 0;
+
+  background: hsla(271, 100%, 50%, 1);
+  background: linear-gradient(
+    225deg,
+    hsla(271, 100%, 50%, 1) 0%,
+    hsla(294, 100%, 50%, 1) 100%
+  );
+  background: -moz-linear-gradient(
+    225deg,
+    hsla(271, 100%, 50%, 1) 0%,
+    hsla(294, 100%, 50%, 1) 100%
+  );
+  background: -webkit-linear-gradient(
+    225deg,
+    hsla(271, 100%, 50%, 1) 0%,
+    hsla(294, 100%, 50%, 1) 100%
+  );
+  box-shadow: 20px 20px 60px #1f2634, -20px -20px 60px #1f2634;
+  border-radius: 50px;
+  font-weight: 600;
+  font-size: 20px;
+
+     &:hover {
+        transform: scale(1.05);
+    transition: all 0.4s ease-in-out;
+    box-shadow:  20px 20px 60px #1F2634,
+    filter: brightness(1);
+    }    
+    
+    
+    @media screen and (max-width: 640px) {
+        padding: 12px 0;
+        font-size: 18px;
+    } 
+    color: white;
+`;
+
+const Navbar = () => {
+  const [open, setOpen] = React.useState(false);
+  const theme = useTheme();
+  return (
+    <Nav>
+      <NavbarContainer>
+        <NavLogo to="/">
+          <a
+            style={{
+              display: "flex",
+              alignItems: "center",
+              color: "white",
+              marginBottom: "20",
+              cursor: "pointer",
+            }}
+          >
+            <DiCssdeck size="3rem" />
+            <Span>Portfolio</Span>
+          </a>
+        </NavLogo>
+        <MobileIcon>
+          <FaBars
+            onClick={() => {
+              setOpen(!open);
+            }}
+          />
+        </MobileIcon>
+        <NavItems>
+          <NavLink href="#about">About</NavLink>
+          <NavLink href="#skills">Skills</NavLink>
+          <NavLink href="#experience">Experience</NavLink>
+          <NavLink href="#projects">Projects</NavLink>
+          <NavLink href="#education">Education</NavLink>
+        </NavItems>
+        <ButtonContainer>
+          <GithubButton href={Bio.github} target="display">Github Profile</GithubButton>
+        </ButtonContainer>
+      </NavbarContainer>
+      {open && (
+        <MobileMenu open={open}>
+          <MobileMenuLinks
+            href="#about"
+            onClick={() => {
+              setOpen(!open);
+            }}
+          >
+            About
+          </MobileMenuLinks>
+          <MobileMenuLinks
+            href="#skills"
+            onClick={() => {
+              setOpen(!open);
+            }}
+          >
+            Skills
+          </MobileMenuLinks>
+          <MobileMenuLinks
+            href="#experience"
+            onClick={() => {
+              setOpen(!open);
+            }}
+          >
+            Experience
+          </MobileMenuLinks>
+          <MobileMenuLinks
+            href="#projects"
+            onClick={() => {
+              setOpen(!open);
+            }}
+          >
+            Projects
+          </MobileMenuLinks>
+          <MobileMenuLinks
+            href="#education"
+            onClick={() => {
+              setOpen(!open);
+            }}
+          >
+            Education
+          </MobileMenuLinks>
+          <GithubButton
+            href={Bio.github}
+            target="display"
+          >
+            Github Profile
+          </GithubButton>
+        </MobileMenu>
+      )}
+    </Nav>
+  );
+};
+
+export default Navbar;
---
+++ src/components/Projects/index.js
@@ -0,0 +1,138 @@
+import React ,{useState} from 'react'
+import styled from 'styled-components';
+import Projectcard from '../Cards/Projectcard';
+import { projects } from "../../data/constants";
+
+const Container = styled.div`
+background:linear-gradient(
+  343.07deg,
+  rgba(132,59,206,0.06) 5.71%,
+  rgba(132,59,206,0) 64.83%
+);
+display: flex;
+flex-direction: column;
+justify-content-center;
+position: relative;
+z-index: 1;
+align-items: center;
+padding: 0px 20px;
+clip-path:polygon(0.0, 100%,100%,100%,98%, 0 100%);
+`;
+const Wrapper = styled.div`
+  position: relative;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  flex-direction: column;
+  width: 100%;
+  max-width: 1350px;
+  gap: 12px;
+  padding:10px 0px 100px 0;
+  @media (max-width: 960px) {
+    flex-direction: column;
+  }
+`;
+
+const Title = styled.div`
+  font-size: 52px;
+  text-align: center;
+  font-weight: 600;
+  margin-top: 20px;
+  color: ${({ theme }) => theme.text_primary};
+  @media (max-width: 768px) {
+    margin-top: 12px;
+    font-size: 32px;
+  }
+`;
+
+const Desc = styled.div`
+  font-size: 18px;
+  text-align: center;
+  font-weight: 600;
+  color: ${({ theme }) => theme.text_secondary};
+  @media (max-width: 768px) {
+    font-size: 16px;
+  }
+`;
+
+const ToggleGroup=styled.div`
+  display:flex;
+  border: 1.5px solid ${({ theme }) => theme.primary};
+  color: ${({ theme }) => theme.primary};
+  font-size:16px;
+  border-radius:12px;
+  font-weight:500;
+  margin:22px 0;
+
+  @media (max-width:768px){
+    font-size:12px;
+  }
+`;
+
+const ToggleButton=styled.div`
+  padding: 8px 18px;
+  cursor: pointer;
+  border-radius:6px;
+
+  ${({active, theme})=>
+    active && 
+    `
+        background-color: ${theme.primary+20};
+    `}
+    &:hover{
+        background-color:${({theme})=>theme.primary+8};
+    }
+    @media (max-width:768px){
+        padding: 6px 8px;
+        border-radius: 4px;
+    }
+`;
+
+const Divider=styled.div`
+    width:1.5px;
+    background-color:${({theme})=>theme.primary};
+`;
+
+const CardContainer=styled.div`
+    display: flex;
+    justify-content:center;
+    align-items:center;
+    flex-wrap:wrap;
+    gap:28px;
+`
+
+const Projects = () => {
+  const [toggle, setToggle]=useState("all");
+  return (
+    <Container id="projects">
+      <Wrapper>
+        <Title>Projects</Title>
+        <Desc>Here are some of my projects</Desc>
+        <ToggleGroup>
+          {toggle === "all" ? (
+            <ToggleButton active value="all" onClick={() => setToggle("all")}>
+              ALL
+            </ToggleButton>
+          ) : (
+            <ToggleButton value="all" onClick={() => setToggle("all")}>ALL</ToggleButton>
+          )}
+          <Divider />
+          {toggle==="web app"?(
+              <ToggleButton active onClick={()=>setToggle("web app")}>WEB APP'S</ToggleButton>
+              ):(
+              <ToggleButton onClick={()=>setToggle("web app")}>WEB APP'S</ToggleButton>
+          )}
+        </ToggleGroup>
+        <CardContainer>
+            {toggle==="all" && projects.map((project)=> <Projectcard project={project}/>)}
+            {projects
+                .filter((item)=>item.category===toggle)
+                .map((project)=> (<Projectcard project={project}/>))
+            }
+        </CardContainer>
+      </Wrapper>
+    </Container>
+  );
+}
+
+export default Projects
